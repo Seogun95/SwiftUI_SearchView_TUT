0f44287470be9b9d7aa578c56df0261c1850790d
SeonJungKim <ksj083895@gmail.com>
2021-02-03 09:37:28 +0500

    Initial Commit

---
+++ SwiftUI_SearchView_TUT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_SearchView_TUT
+//
+//  Created by 김선중 on 2021/02/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
