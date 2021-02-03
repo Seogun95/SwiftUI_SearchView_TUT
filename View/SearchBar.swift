215759802a2c930eb15fb8609e1479861165f29f
SeonJungKim <ksj083895@gmail.com>
2021-02-03 12:29:23 +0500

    1. 검색창을 만들어 줍니다.

---
+++ View/SearchBar.swift
@@ -0,0 +1,39 @@
+//
+//  SearchBar.swift
+//  SwiftUI_SearchView_TUT
+//
+//  Created by 김선중 on 2021/02/03.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    
+    @Binding var text: String
+
+    var body: some View {
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+
+                TextField("Search", text: $text)
+                    .foregroundColor(.primary)
+
+                if !text.isEmpty {
+                    Button(action: {
+                        self.text = ""
+                    }) {
+                        Image(systemName: "xmark.circle.fill")
+                    }
+                } else {
+                    EmptyView()
+                }
+            }
+            .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
+            .foregroundColor(.secondary)
+            .background(Color(.secondarySystemBackground))
+            .cornerRadius(10.0)
+        }
+        .padding(.horizontal)
+    }
+}
