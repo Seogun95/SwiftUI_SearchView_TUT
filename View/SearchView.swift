847d0f1e967dea0e48d3bcabda60b81e710daa27
SeonJungKim <ksj083895@gmail.com>
2021-02-04 13:20:22 +0500

    불필요한 부분 삭제

--- View/SearchView.swift
+++ View/SearchView.swift
@@ -51,15 +51,6 @@ struct ContentView_Previews: PreviewProvider {
         SearchView()
     }
 }
-//화면 터치시 키보드 숨김
-extension UIApplication {
-    func endEditing(_ force: Bool) {
-        self.windows
-            .filter{$0.isKeyWindow}
-            .first?
-            .endEditing(force)
-    }
-}
 
 #if canImport(UIKit)
 extension View {
